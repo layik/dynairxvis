10217d21cf6bb328fca1297ac9a1a28e45f9127e
Layik Hama <layik.hama@gmail.com>
2024-04-24 07:33:11 +0100

    tests: why windows fails++

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -1,21 +1,21 @@
-# from unittest.mock import patch
-# import os
-# from dynairxvis.plot import plot
-# from .test_utils import CATEGORIES, VALUES
+from unittest.mock import patch
+import os
+from dynairxvis.plot import plot
+from .test_utils import CATEGORIES, VALUES
 
 
-# @patch('matplotlib.pyplot.show')
-# def test_plot_saves_file(mock_show):
-#     # When function calls plt.show(), it won't actually display the plot.
-#     # Instead, plt.show() does nothing during this test,
-#     # allowing the test to proceed without interruption.
+@patch('matplotlib.pyplot.show')
+def test_plot_saves_file(mock_show):
+    # When function calls plt.show(), it won't actually display the plot.
+    # Instead, plt.show() does nothing during this test,
+    # allowing the test to proceed without interruption.
 
-#     # Test if the plot has been saved to a file
-#     # This requires modifying the radar function to save the plot
-#     filename = "test_plot.png"
-#     plot('radar', CATEGORIES, VALUES, filename=filename)
-#     assert os.path.isfile(filename), "Plot file was not created"
-#     os.remove(filename)  # Clean up the file after test
+    # Test if the plot has been saved to a file
+    # This requires modifying the radar function to save the plot
+    filename = "test_plot.png"
+    plot('radar', CATEGORIES, VALUES, filename=filename)
+    assert os.path.isfile(filename), "Plot file was not created"
+    os.remove(filename)  # Clean up the file after test
 
 
 # @patch("builtins.print")
